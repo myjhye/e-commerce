5471467d73a18384ad2668e65adae6d0a339d733
myjhye <t01087633799@gmail.com>
2025-07-14 01:19:57 +0900

    feat: 제품 이미지 추가 및 카드 UI에 이미지 연동

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,7 +1,25 @@
+import { Col, Row, Card } from "react-bootstrap";
+import products from "../products";
+
 export default function HomeScreen() {
-    return (
-        <div>
-            <h1>Latest Products</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Card className="my-3 p-3 rounded">
+              <Card.Img src={product.image} variant="top" />
+              <Card.Body>
+                <Card.Title as="div">
+                  <strong>{product.name}</strong>
+                </Card.Title>
+                <Card.Text as="h3">${product.price}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
 }
