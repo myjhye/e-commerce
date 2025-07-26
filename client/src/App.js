ef42d997e7c00a396a794a6888e02721d7d2b8c3
myjhye <t01087633799@gmail.com>
2025-07-26 15:06:19 +0900

    feat: JWT 토큰 자동 갱신 시스템 구현

--- client/src/App.js
+++ client/src/App.js
@@ -7,23 +7,29 @@ import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen';
 import LoginScreen from './screens/LoginScreen';
 
+import { Provider } from 'react-redux'
+import store from './store'
+import './utils/axiosConfig' // axios interceptor 설정
+
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Header />
-        <main className="py-3">
-          <Container>
-            <Routes>
-              <Route path='/' element={<HomeScreen />} />
-              <Route path='/product/:id' element={<ProductScreen />} />
-              <Route path='/login' element={<LoginScreen />} />
-            </Routes>
-          </Container>
-        </main>
-        <Footer />
-      </Router>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Router>
+          <Header />
+          <main className="py-3">
+            <Container>
+              <Routes>
+                <Route path='/' element={<HomeScreen />} />
+                <Route path='/product/:id' element={<ProductScreen />} />
+                <Route path='/login' element={<LoginScreen />} />
+              </Routes>
+            </Container>
+          </main>
+          <Footer />
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
