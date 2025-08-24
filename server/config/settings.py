ad089abd35faec75fb8f7c836fd72344d0d4680f
myjhye <t01087633799@gmail.com>
2025-08-24 22:17:23 +0900

    fix

--- server/config/settings.py
+++ server/config/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'http://54.180.108.241/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # CORS 설정
