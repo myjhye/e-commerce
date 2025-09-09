dd2852b253dbb97f56a5ff4383aadd586fcb1c47
myjhye <t01087633799@gmail.com>
2025-09-09 23:17:47 +0900

    fix: 주문 관련 모델의 가격 필드 자릿수 오류 수정

--- server/base/models.py
+++ server/base/models.py
@@ -35,9 +35,9 @@ class Review(models.Model):
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     paymentMethod = models.CharField(max_length=200, null=True, blank=True)
-    taxPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
-    shippingPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
-    totalPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    taxPrice = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    shippingPrice = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    totalPrice = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     isPaid = models.BooleanField(default=False)
     paidAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
     isDelivered = models.BooleanField(default=False)
@@ -54,7 +54,7 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
     qty = models.IntegerField(null=True, blank=True, default=0)
-    price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     image = models.CharField(max_length=200, null=True, blank=True)
     _id = models.AutoField(primary_key=True, editable=False)
 
